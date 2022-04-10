a1680459319dee01ec748d06d4bb5ed55e252398
Elroy Hui <huielroy@hotmail.com>
2022-04-10 14:00:13 -0400

    +created sumOrderTotal function (untested)

--- db/queries/03_menu_item_queries.js
+++ db/queries/03_menu_item_queries.js
@@ -13,3 +13,30 @@ const getAllMenuItems = () => {
 };
 
 exports.getAllMenuItems = getAllMenuItems;
+
+const sumOrderTotal = (order) => {
+  const menuIDs = Object.keys(order);
+  const quantity = Object.values(order);
+  let total = 0;
+
+  for (let i = 0; i < menuIDs.length; i++) {
+    const menuObj = sumOrderTotalQuery(menuIDs[i]);
+    total += menuObj.price * quantity[i];
+  }
+  return total;
+};
+
+exports.sumOrderTotal = sumOrderTotal;
+
+const sumOrderTotalQuery = (order) => {
+  // 1
+
+  // 2
+  let queryString = `
+  SELECT *
+  FROM menu_items
+  WHERE id = $1
+  `;
+
+  return db.query(queryString, queryParams).then((res) => res.rows[0]);
+};
