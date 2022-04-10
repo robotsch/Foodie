5cf1840e6d71d2bb1cbefbde595335bcaf7ce248
Elroy Hui <huielroy@hotmail.com>
2022-04-10 03:35:37 -0400

    +created categories and menu_items table data

--- db/schema/01_general.sql
+++ db/schema/01_general.sql
@@ -24,6 +24,7 @@ CREATE TABLE categories (
 
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(55) NOT NULL,
   price SMALLINT NOT NULL,
   description VARCHAR(255) NOT NULL DEFAULT 'A delicious item sure to suit your every need!',
   image_url VARCHAR(255) NOT NULL DEFAULT 'https://toppng.com/uploads/preview/clipart-free-seaweed-clipart-draw-food-placeholder-11562968708qhzooxrjly.png',
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (category) VALUES ('Appetizers');
+INSERT INTO categories (category) VALUES ('Mains');
+INSERT INTO categories (category) VALUES ('Desserts');
+INSERT INTO categories (category) VALUES ('Drinks');
+
