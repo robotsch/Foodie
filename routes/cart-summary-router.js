0c815a63eb824de7574c054bb606f1ee5d33159e
robotsch <christianhumble22@gmail.com>
2022-04-10 15:52:20 -0400

    remove extra response action

--- routes/cart-summary-router.js
+++ routes/cart-summary-router.js
@@ -11,24 +11,15 @@ const menuQueries = require('../db/queries/03_menu_item_queries');
 
 router.get("/", (req, res) => {
   const order = req.session.order;
-  const serviceFee = 100;
   if (order) {
-
-    // TODO: list menu items on top of cart
-
     menuQueries.sumOrderTotal(order)
-
       .then((data) => {
-        const subtotal = data.rows[0];
-        const tax = subtotal * 0.13;
-        const total = subtotal + serviceFee + tax;
-        res.json({ "Subtotal": subtotal, "Service Fee": serviceFee, "Tax": tax, "Total": total });
+        res.json(data.row[0])
       })
       .catch((err) => {
         res.status(500).send('Failed to get cart');
       });
   }
-  res.send('Hello');
 });
 
 module.exports = router;
