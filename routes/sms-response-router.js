4967230d58f99e8aca092da0520ab6d907f64665
robotsch <christianhumble22@gmail.com>
2022-04-11 14:54:25 -0400

    add bad input check

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const orderQueries = require("../db/queries/04_orders_queries")
 const twilioClient = require("../lib/twilio");
 
 router.post("/", (req, res) => {
   const [orderId, estimatedTime] = req.body.split(' ')
-  console.log(orderId, estimatedTime)
+  if(Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
+    return twilioClient.messages
+    .create({body: 'Please respond with the order number followed by estimated time in minutes',
+      from: process.env.APP_PHONE, to: process.env.RESTAURANT_PHONE})
+    .then((messages) => {
+      console.log(messages)
+    })
+  } 
+  orderQueries.acceptOrder(Number(orderId), Number(estimatedTime))
 });
 
 module.exports = router;
