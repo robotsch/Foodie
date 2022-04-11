630d0598e5da84226a59939183b18b2a31936bfa
robotsch <christianhumble22@gmail.com>
2022-04-11 15:04:02 -0400

    fix opposite case bug

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const orderQueries = require("../db/queries/04_orders_queries")
+const orderQueries = require("../db/queries/04_orders_queries");
 const twilioClient = require("../lib/twilio");
 
 router.post("/", (req, res) => {
-  console.log(req)
-  const [orderId, estimatedTime] = req.body.Body.split(' ')
-  if(Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
-    return twilioClient.messages
-    .create({body: 'Please respond with the order number followed by estimated time in minutes',
-      from: process.env.APP_PHONE, to: process.env.RESTAURANT_PHONE})
-    .then((messages) => {
-      console.log(messages)
+  const [orderId, estimatedTime] = req.body.Body.split(" ");
+  console.log();
+  if (Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
+    orderQueries.acceptOrder(Number(orderId), Number(estimatedTime));
+    return res.send("Success");
+  }
+  twilioClient.messages
+    .create({
+      body: "Please respond with the order number followed by estimated time in minutes",
+      from: process.env.APP_PHONE,
+      to: process.env.RESTAURANT_PHONE,
     })
-  } 
-  orderQueries.acceptOrder(Number(orderId), Number(estimatedTime))
+    .then((messages) => {
+      console.log(messages);
+    });
 });
 
 module.exports = router;
