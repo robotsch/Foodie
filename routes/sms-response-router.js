9231f06cad0522c3c64b9931f37e4ac1c26c5a87
robotsch <christianhumble22@gmail.com>
2022-04-11 14:35:52 -0400

    sms response setup

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const menuQueries = require("../db/queries/03_menu_item_queries");
 const twilioClient = require("../lib/twilio");
 
 router.post("/", (req, res) => {
-  console.log(req.body)
+  const [orderId, estimatedTime] = req.body.split(' ')
+  console.log(orderId, estimatedTime)
 });
 
 module.exports = router;
