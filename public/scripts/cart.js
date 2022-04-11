b88ec970d71ede0e5c224a66d55f664e45e0a9bc
otrachea <francesca.ho459@gmail.com>
2022-04-11 21:33:25 +0000

    Cart page uses sessionStorage data to generate cart and totals

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -18,31 +18,56 @@ $(() => {
   `);
   };
 
-  const createTotals = function (cartData) {
+  const createTotals = function (totals) {
     return $(`
     <div id="totals-container">
       <div>
         <div>Subtotal</div>
-        <div>$${1}</div>
+        <div>$${totals.subtotal}</div>
       </div>
       <div>
         <div>Serivce Fee</div>
-        <div>$1.00</div>
+        <div>$${(totals.serviceFee).toFixed(2)}</div>
       </div>
       <div>
         <div>Tax</div>
-        <div>$${1}</div>
+        <div>$${(totals.tax).toFixed(2)}</div>
       </div>
       <div>
         <div>Total</div>
-        <div>$${1}</div>
+        <div>$${(totals.total).toFixed(2)}</div>
       </div>
     </div>   
     `);
   };
 
   const renderCart = function (cartData) {
-    $(`h3`).after(createItems({ name: "Spring Rolls", quantity: 3, price: 150 }), createTotals());
+    if (Object.keys(cartData).length === 0) {
+      $(`#cart-container > h3`).after(`
+      <div class="my-5">
+        <h4>No items in cart</h4>
+      </div>
+      `);
+      return;
+    }
+
+    const checkoutBtn = $(`#cart-container > button:last-child`);
+
+    let sum = 0;
+
+    for (const menuItem of Object.values(cartData)) {
+      sum += menuItem.price * menuItem.quantity;
+      checkoutBtn.before(createItems(menuItem));
+    }
+
+    checkoutBtn.before(createTotals({
+      subtotal: sum / 100,
+      serviceFee: 1,
+      tax: sum * 0.13 / 100,
+      total: sum * 1.13 / 100
+    }));
+
+    checkoutBtn.before("<hr>");
   };
 
   const orders = JSON.parse(sessionStorage.getItem('orders'));
@@ -53,6 +78,7 @@ $(() => {
     data: orders,
     success: function (response) {
       console.log(response);
+      renderCart(response);
     },
     err: function (err) {
       console.log(err.message);
