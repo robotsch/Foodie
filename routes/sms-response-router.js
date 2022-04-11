2cdb13489a572a37695a864be1b23f15200ce8d1
robotsch <christianhumble22@gmail.com>
2022-04-11 14:59:36 -0400

    fix request body bug

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -5,7 +5,7 @@ const twilioClient = require("../lib/twilio");
 
 router.post("/", (req, res) => {
   console.log(req)
-  const [orderId, estimatedTime] = req.body.split(' ')
+  const [orderId, estimatedTime] = req.body.Body.split(' ')
   if(Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
     return twilioClient.messages
     .create({body: 'Please respond with the order number followed by estimated time in minutes',
