dc047ae4bd3b3ba4bab4db5fa68747f1b1162452
robotsch <christianhumble22@gmail.com>
2022-04-12 15:17:00 -0400

    refactor register/login into auth-utils file

---
+++ utils/auth-utils.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs");
+const userQueries = require('../db/queries/01_user_queries');
+
+/**
+ * Creates a new user in the database with the provided info
+ * @returns the newly created user's ID and sets the session
+ */
+
+const registerUser = function(userObj) {
+  
+  userQueries.addUser({
+    email: userObj.email,
+    pw: bcrypt.hashSync(userObj.password, 10),
+    fname: userObj.firstName,
+    lname: userObj.lastName,
+    phone: userObj.phone
+  })
+    .then((newUser) => {
+      return newUser.id;
+    })
+    .catch((err) => {
+      console.log('Registration failed: ', err);
+    });
+};
+
+module.exports = registerUser;
