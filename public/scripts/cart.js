cf234f83213e756b26874fca445b937bdf09a420
otrachea <francesca.ho459@gmail.com>
2022-04-12 05:00:23 +0000

    Set quantity modal has full functionality and your cart page items update appropriately if item is removed or quantity is changed

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,7 +2,7 @@ $(() => {
 
   const createItem = function (orderData) {
     return $(`
-    <div class="item-container">
+    <div class="item-container" id="item-${orderData.id}">
       <div>
         <div class="px-2 py-1 mx-2 text-center">${orderData.quantity}</div>
         <div>${orderData.name}</div>
@@ -92,16 +92,16 @@ $(() => {
       `);
       return;
     }
-    
+
     let sum = 0;
-    
+
     for (const menuItem of Object.values(cartData)) {
       let newQuantity = 1;
-      
+
       sum += menuItem.price * menuItem.quantity;
       $("#cart-container > h3").after(createItem(menuItem));
-      
-      $(`#edit-quantity-btn-${menuItem.id}`).on("click", function (e) {
+
+      $(`#edit-quantity-btn-${menuItem.id}`).on("click", function () {
         const modal = $(`#edit-quantity-modal`);
         $('.modal-title').text(menuItem.name);
         $('#desc').text(menuItem.description);
@@ -109,38 +109,63 @@ $(() => {
         $(".quantity").text(menuItem.quantity);
         $("#total").text(`$${(menuItem.price * menuItem.quantity / 100).toFixed(2)}`);
 
-        $("#minus-quantity-btn").unbind().on("click", function (e) {
-          if (menuItem.quantity > 0) {
+        $("#minus-quantity-btn").unbind().on("click", function () {
+          if (menuItem.quantity > 1) {
             menuItem.quantity--;
             $(".quantity").text(menuItem.quantity);
+            $("#total").text(`$${(menuItem.price * menuItem.quantity / 100).toFixed(2)}`);
           }
         });
 
-        $("#plus-quantity-btn").unbind().on("click", function (e) {
+        $("#plus-quantity-btn").unbind().on("click", function () {
           if (menuItem.quantity < 100) {
             menuItem.quantity++;
             $(".quantity").text(menuItem.quantity);
+            $("#total").text(`$${(menuItem.price * menuItem.quantity / 100).toFixed(2)}`);
           }
         });
 
-        $("#set-quantity-btn").on("click", function(e) {
+        $("#set-quantity-btn").on("click", function () {
           const sessionCart = JSON.parse(sessionStorage.getItem('orders'));
-          // sessionCart[menuItem.id] 
-          // sessionCart[menuItem.id] = 
-          // idk if commenting/uncommenting this line does much
-          // delete cartData[menuItem.id];
-          sessionStorage.setItem('orders', JSON.stringify(sessionCart));
-        })
 
-        $("#remove-btn").on("click", function(e) {
-          const sessionCart = JSON.parse(sessionStorage.getItem('orders'));
-          // delete sessionCart[menuItem.id];
-          console.log($(this))
+          // Gets new quantity
+          const newQuantity = parseInt($(this).parent().prev().children("div:last-child").children("span").text());
+
+          // Updates new quantity in sessionStorage
+          sessionCart[menuItem.id] = newQuantity;
+
+          // Updates quantity in yellow div on 'Your Cart' page to new quantity
+          $(`#item-${menuItem.id}`).children("div:first-child").children("div:first-child").text(newQuantity);
+
+          // Updates total price for each item on 'Your Cart' page after quantity changes
+          $(`#item-${menuItem.id}`).children("div:last-child").children("div:last-child").text(`$${(menuItem.price * menuItem.quantity / 100).toFixed(2)}`);
+
           // idk if commenting/uncommenting this line does much
           // delete cartData[menuItem.id];
+
+          // Updates sessionStorage
           sessionStorage.setItem('orders', JSON.stringify(sessionCart));
-        })
-      
+        });
+
+        $("#remove-btn").on("click", () => {
+          const sessionCart = JSON.parse(sessionStorage.getItem('orders'));
+
+          // Deletes from sessionStorage
+          delete sessionCart[menuItem.id];
+
+          // Deletes horizontal line under item entry in cart from DOM
+          $(`#item-${menuItem.id}`).next().remove();
+
+          // Deletes item entry in cart from DOM
+          $(`#item-${menuItem.id}`).remove();
+
+          // idk if commenting/uncommenting this line does much
+          // delete cartData[menuItem.id];
+
+          // Updates sessionStorage
+          sessionStorage.setItem('orders', JSON.stringify(sessionCart));
+        });
+
         modal.modal("toggle");
       });
 
