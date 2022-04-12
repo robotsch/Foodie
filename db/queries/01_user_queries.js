82fe9ea7217fa0cb363a1fc2b04da84e6ecf111c
robotsch <christianhumble22@gmail.com>
2022-04-12 16:23:26 -0400

    remove other log lines

--- db/queries/01_user_queries.js
+++ db/queries/01_user_queries.js
@@ -21,7 +21,6 @@ const getUserWithId = function (id) {
   return db
     .query(`SELECT * FROM users WHERE id = $1`, [id])
     .then((result) => {
-      console.log("result: ", result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
@@ -45,7 +44,6 @@ const addUser = function (user) {
       values
     )
     .then((result) => {
-      console.log("result: ", result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
