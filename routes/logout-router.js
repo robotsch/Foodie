ba9fc1639a6a9215c51b99d363b2a6fa786d0bea
robotsch <christianhumble22@gmail.com>
2022-04-13 14:04:52 -0400

    add logout router

---
+++ routes/logout-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Destroy session and redirect user to main page
+router.get("/", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/")
+  })
+})
