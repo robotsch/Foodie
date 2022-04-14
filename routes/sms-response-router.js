e4db751e6f0677f249291cd74778abee3f7cef9e
robotsch <christianhumble22@gmail.com>
2022-04-14 13:00:46 -0400

    add auto-timeout for orders

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -7,6 +7,8 @@ const twilioClient = require("../lib/twilio");
  * Handle incoming Twilio webhook triggered by incoming SMS from restaurant
  * Parses orderId and estimatedTime, and updates database with those values
  * Sends SMS to customer's phone when the order is accepted
+ * Sets an automatic timeout for the order to be completed equal to
+ *  2x the estimated order time + 15 minutes
  */
 
 router.post("/", (req, res) => {
@@ -14,8 +16,13 @@ router.post("/", (req, res) => {
   const areaCode = process.env.USER_AREA_CODE || '+1'
   if (Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
     orderQueries.acceptOrder(Number(orderId), Number(estimatedTime));
+    
     orderQueries.getOrderPhone(Number(orderId))
       .then((orderPhone) => {
+        setTimeout(() => {
+          orderQueries.completeOrder(Number(orderId))
+        }, (Number(estimatedTime)* 60 * 1000 * 2) + 900000)
+        
         twilioClient.messages.create({
           body: `Your order from Golden Wok has been confirmed! It will be ready for pickup in approximately ${estimatedTime} minutes`,
           from: process.env.APP_PHONE,
