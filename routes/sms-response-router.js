44e8d8f8ddeccb68e5f3b7511922e71f1cdc82c8
robotsch <christianhumble22@gmail.com>
2022-04-14 12:37:14 -0400

    add sms send to user on order confirmation

--- routes/sms-response-router.js
+++ routes/sms-response-router.js
@@ -12,6 +12,14 @@ router.post("/", (req, res) => {
   const [orderId, estimatedTime] = req.body.Body.split(" ");
   if (Number(orderId) % 1 === 0 || Number(estimatedTime % 1 === 0)) {
     orderQueries.acceptOrder(Number(orderId), Number(estimatedTime));
+    orderQueries.getOrderPhone(Number(orderId))
+      .then((orderPhone) => {
+        twilioClient.messages.create({
+          body: `Your order from ${restaurant} has been confirmed! It will be ready for pickup in approximately ${estimatedTime} minutes`,
+          from: process.env.APP_PHONE,
+          to: orderPhone
+        });
+      })
     return res.send("Success");
   }
   twilioClient.messages
